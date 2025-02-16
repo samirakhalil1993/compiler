34276ca74bad22b19c2fb2d6205fe33be8b40c23
samir akhalil <samir.akhalil.sy@gmail.com>
2025-02-16 19:47:00 +0100

    fixet semantic

--- Semantic.cc
+++ Semantic.cc
@@ -1,35 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "Semantic.hh"  // Include the header file
 
-// Define a structure for a variable record (stores information about variables)
-typedef struct VariableRecord {
-    char name[50];                // Name of the variable
-    char type[20];                // Data type (e.g., int, float, string)
-    int scope_level;              // Scope level of the variable (for scoping rules)
-    struct VariableRecord* next;  // Pointer to the next variable in the list
-} VariableRecord;
 
-// Define a structure for a method record (stores information about methods in a class)
-typedef struct MethodRecord {
-    char name[50];                // Name of the method
-    char returnType[20];          // Return type of the method
-    VariableRecord* parameters;   // Pointer to a list of parameters for the method
-    struct MethodRecord* next;    // Pointer to the next method in the class
-} MethodRecord;
-
-// Define a structure for a class record (stores information about classes)
-typedef struct ClassRecord {
-    char name[50];                // Name of the class
-    VariableRecord* fields;       // Pointer to a list of class fields (member variables)
-    MethodRecord* methods;        // Pointer to a list of methods in the class
-    struct ClassRecord* next;     // Pointer to the next class in the symbol table
-} ClassRecord;
-
-// Symbol Table structure (stores all classes)
-typedef struct SymbolTable {
-    ClassRecord* classes;         // Pointer to the first class in the symbol table
-} SymbolTable;
 
 // Function to add a class to the symbol table
 void addClass(SymbolTable* table, const char* name) {
@@ -99,14 +73,6 @@ ClassRecord* lookupClass(SymbolTable* table, const char* name) {
     return NULL;  // Class not found
 }
 
-// Define a structure for an Abstract Syntax Tree (AST) node
-typedef struct ASTNode {
-    char type[20];        // Type of node (e.g., "class", "method", "variable")
-    char name[50];        // Name of the node
-    char dataType[20];    // Data type (for variables and methods)
-    struct ASTNode* child; // Pointer to the first child node (sub-nodes)
-    struct ASTNode* next;  // Pointer to the next sibling node
-} ASTNode;
 
 // Function to traverse an Abstract Syntax Tree (AST) and populate the symbol table
 void traverseAST(ASTNode* node, SymbolTable* table) {
